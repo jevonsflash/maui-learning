bbb40ebb3fa3ac24768325f48429fe019540519a
小 林 <jevonsflash@hotmail.com>
2023-05-12 18:47:04 +0800

    添加项目文件。

---
+++ Lession1/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Lession1.ViewModels;
+
+namespace Lession1;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+        this.BindingContext=new MainPageViewModel();
+
+    }
+}
+
